ccc07f838dcc240a69e683ffa389d89a2dac5919
pzach01 <peter.douglas.zach@gmail.com>
2020-05-08 23:32:29 -0400

    add new item modal

--- src/app/_models/arrangement.ts
+++ src/app/_models/arrangement.ts
@@ -1,9 +1,6 @@
-import { Bin } from "./bin"
-
 export class Arrangement {
     public id: Number;
     public owner: String;
     public bins: String;
     public boxes: String;
-    public binLayout: Bin[]
 }
