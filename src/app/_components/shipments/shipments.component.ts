db06ee1cbd321971909945a517b37373663f24e1
pzach01 <peter.douglas.zach@gmail.com>
2020-06-15 09:44:06 -0400

    added setting htmls w/o functionality

--- src/app/_components/shipments/shipments.component.ts
+++ src/app/_components/shipments/shipments.component.ts
@@ -40,7 +40,7 @@ export class ShipmentsComponent implements OnInit {
     const dialogRef = this.newShipmentDialog.open(NewShipmentComponent, {
       panelClass: 'custom-dialog-container',
       width: '100%',
-      height: '90%'
+      height: '90vh'
     });
 
     dialogRef.afterClosed().subscribe(newShipment => {
