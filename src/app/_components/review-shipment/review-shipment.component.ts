98c85da9ad778cbae3034b5d0a8307d2b8023939
pzach01 <peter.douglas.zach@gmail.com>
2020-06-24 14:33:54 -0400

    formatting changes

--- src/app/_components/review-shipment/review-shipment.component.ts
+++ src/app/_components/review-shipment/review-shipment.component.ts
@@ -1,24 +1,29 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Item } from 'src/app/_models/item';
 import { Container } from 'src/app/_models/container';
 import { MatTableDataSource } from '@angular/material/table';
+import { AuthenticationService } from 'src/app/_services';
 
 @Component({
   selector: 'app-review-shipment',
   templateUrl: './review-shipment.component.html',
   styleUrls: ['./review-shipment.component.scss']
 })
-export class ReviewShipmentComponent implements OnChanges {
+export class ReviewShipmentComponent implements OnChanges, OnInit {
   @Input() selectedItems: Item[];
   @Input() selectedContainers: Container[];
-  multiBinPack: boolean = false;
+  currentUser = this.authenticationService.currentUserValue;
+  multiBinPack: boolean = this.currentUser.multiBinPack;
   itemsDataSource = new MatTableDataSource(this.selectedItems);
   containersDataSource = new MatTableDataSource(this.selectedContainers);
   itemsDisplayedColumns: string[] = ['sku', 'description', 'qty'];
   containersDisplayedColumns: string[] = ['sku', 'description'];
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
+  ngOnInit() {
+    this.authenticationService.currentUser.subscribe((currentUser) => this.currentUser = currentUser)
+  }
   ngOnChanges() {
     this.itemsDataSource.data = this.selectedItems;
     this.containersDataSource.data = this.selectedContainers;
