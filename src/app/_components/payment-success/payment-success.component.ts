e346638595b62e100cbd909dcd94a34f98e4aa0d
pzach01 <peter.douglas.zach@gmail.com>
2020-09-14 16:44:19 -0400

    added retry to payment success

--- src/app/_components/payment-success/payment-success.component.ts
+++ src/app/_components/payment-success/payment-success.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SubscriptionInfo } from 'src/app/_models';
 import { SubscriptionsService } from 'src/app/_services/subscriptions.service';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-payment-success',
@@ -8,12 +10,28 @@ import { SubscriptionsService } from 'src/app/_services/subscriptions.service';
   styleUrls: ['./payment-success.component.scss']
 })
 export class PaymentSuccessComponent implements OnInit {
+  subscription: Subscription;
   subscriptionInfo: SubscriptionInfo = this.subscriptionService.currentSubscriptionInfoValue;
-
   constructor(public subscriptionService: SubscriptionsService) { }
 
   ngOnInit(): void {
-    this.subscriptionService.getSubscriptionInfo().subscribe(currentSubscription => { this.subscriptionInfo = currentSubscription; console.log(currentSubscription) })
+
+    const source = interval(3500);
+    this.subscription = source.subscribe(() => this.checkSubscription());
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  checkSubscription() {
+    console.log("checking")
+    this.subscriptionService.getSubscriptionInfo().subscribe(subscriptionInfo => {
+      this.subscriptionInfo = subscriptionInfo;
+      if (subscriptionInfo.userHasViewRights) {
+        console.log("userHasViewRights")
+      }
+      console.log(subscriptionInfo);
+    })
   }
 
 }
