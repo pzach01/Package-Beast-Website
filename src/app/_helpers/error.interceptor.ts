9dbb06f0658abd1d343cc8489c258ebecae93fa0
pzach01 <peter.douglas.zach@gmail.com>
2020-12-29 18:31:07 -0500

    added auto logout on 401 unauthorized responses

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -4,18 +4,21 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { AuthenticationService } from '../_services';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) { }
+    constructor(private authenticationService: AuthenticationService, private router: Router) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
             if (err instanceof HttpErrorResponse) {
                 console.log("eee", err)
                 if (err.status === 401) {
-                    // auto logout if 401 response returned from api
+                    // auto logout and redirect if 401 response returned from api
                     this.authenticationService.logout();
+                    this.router.navigate([{ outlets: { primary: 'login', view: null } }]);
+
                     // location.reload(true);
                 }
 
