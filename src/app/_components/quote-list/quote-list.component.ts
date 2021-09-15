c7322cdb51717df7de224afe2af6a65148bff50e
pzach01 <peter.douglas.zach@gmail.com>
2021-09-14 21:38:40 -0400

    removed quote list observable shipment logic. too complicato

--- src/app/_components/quote-list/quote-list.component.ts
+++ src/app/_components/quote-list/quote-list.component.ts
@@ -21,25 +21,29 @@ export class QuoteListComponent implements OnInit {
   faCheckCircle = faCheckCircle;
   displayedColumns: string[] = ["selectedIcon", "carrier", "cost", "daysToShip", "serviceDescription", "containerSku", "containerDescription"];
   dataSource;
+  shipment: Shipment;
   loading: boolean = false
   userHasQuotes = true
-  shipment: Observable<Shipment>
+
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private shipmentsService: ShipmentsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
 
-    this.shipment = of(window.history.state.shipment);
+    // this.shipment = of(window.history.state.shipment);
 
-    if (!window.history.state.quotes) {
-      // No quotes. Better get a shipment from the backend
-      const shipmentId = +this.route.parent.snapshot.params['id'];
-      this.shipment = this.shipmentsService.getShipmentById(shipmentId)
-    }
-    this.shipment.subscribe(shipment => {
+    const shipmentId = +this.route.parent.snapshot.params['id'];
+
+    // if (!window.history.state.quotes) {
+    //   // No quotes. Better get a shipment from the backend
+    //   const shipmentId = +this.route.parent.snapshot.params['id'];
+    //   this.shipment = this.shipmentsService.getShipmentById(shipmentId)
+    // }
+    this.shipmentsService.getShipmentById(shipmentId).subscribe(shipment => {
       console.log(shipment)
       if (shipment) {
+        this.shipment = shipment
         this.dataSource = new MatTableDataSource(shipment.quotes);
         this.dataSource.sort = this.sort;
       }
@@ -47,10 +51,10 @@ export class QuoteListComponent implements OnInit {
   }
 
   openQuoteDetail(quote: Quote) {
-    this.shipment.subscribe(shipment => {
-      this.shipmentsService.setLastSelectedQuote(shipment, quote).subscribe(() => {
-        this.router.navigate(['./', { outlets: { view: ['shipments', +this.route.parent.snapshot.params['id'], 'quotes', quote.id] } }]);
-      }), e => console.log(e)
-    })
+
+    this.shipmentsService.setLastSelectedQuote(this.shipment, quote).subscribe(() => {
+      this.router.navigate(['./', { outlets: { view: ['shipments', +this.route.parent.snapshot.params['id'], 'quotes', quote.id] } }]);
+    }), e => console.log(e)
+
   }
 }
