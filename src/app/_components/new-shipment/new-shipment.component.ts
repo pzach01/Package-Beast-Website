cfb0fb97073a9c1933796f2a523a4c80c42c6720
pzach01 <peter.douglas.zach@gmail.com>
2022-11-11 22:13:13 -0500

    invalid address error handling

--- src/app/_components/new-shipment/new-shipment.component.ts
+++ src/app/_components/new-shipment/new-shipment.component.ts
@@ -37,7 +37,7 @@ export class NewShipmentComponent implements OnInit {
   loading = false;
   interval;
   spinnerValue = 0;
-  timeoutDuration = 70;
+  timeoutDuration = 30;
   dwellTime = 1000; //ms
   allowAnalysis: boolean = false;
   newShipmentTitle: string = "My New Shipment";
@@ -202,16 +202,18 @@ export class NewShipmentComponent implements OnInit {
         this.fastForwardSpinner(shipment)
       }, error => {
         console.log(error)
+        this.pauseSpinnerInterval();
+        this.fastForwardSpinner()
         if (error.detail == "Not found.") {
-          this.fastForwardSpinner()
           this.close(); this.openCreateFailDialog();
         } else if (error.message == 'invalid to address') {
-          this.fastForwardSpinner();
           this.openInvalidAddressDialog('toAddress');
           this.myStepper.selectedIndex = 1;
         } else if (error.message == 'invalid from address') {
-          this.fastForwardSpinner();
           this.openInvalidAddressDialog('fromAddress');
+          this.myStepper.selectedIndex = 0;
+        } else if (error.message == 'same from and to addresses') {
+          this.openInvalidAddressDialog('sameFromAndToAddresses');
           this.myStepper.selectedIndex = 0;
         }
       }
