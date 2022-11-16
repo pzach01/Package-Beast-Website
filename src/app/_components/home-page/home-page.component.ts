6132e49aba819fcc0d4219a991e8c0cfc4491813
pzach01 <peter.douglas.zach@gmail.com>
2022-11-15 23:04:59 -0500

    formatted home and registration pages, added animation

--- src/app/_components/home-page/home-page.component.ts
+++ src/app/_components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { faFacebookSquare, faTwitterSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
 
@@ -12,10 +12,35 @@ export class HomePageComponent implements OnInit {
   faFacebookSquare = faFacebookSquare;
   faTwitterSquare = faTwitterSquare;
   faYoutubeSquare = faYoutubeSquare;
+  animatecompbox = false;
+  animaterenderingimage = false;
 
   constructor(private router: Router) { }
 
+  @ViewChild('compwithbox') compwithbox: ElementRef;
+  @ViewChild('renderingimage1') renderingimage1: ElementRef;
   ngOnInit(): void {
+
+    document.addEventListener('wheel', (e) => {
+      if (this.isElementInViewport(this.compwithbox.nativeElement)) {
+        this.animatecompbox = true
+      }
+      if (this.isElementInViewport(this.renderingimage1.nativeElement)) {
+        this.animaterenderingimage = true
+      }
+    });
+  }
+
+  isElementInViewport(el) {
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
   }
 
   goToRegister() {
