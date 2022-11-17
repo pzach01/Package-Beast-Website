12ede108b91708e8c59a5ad50b21ba8c777623bc
pzach01 <peter.douglas.zach@gmail.com>
2022-11-17 00:18:43 -0500

    changed scroll-wheel event emitter to timer

--- src/app/_components/home-page/home-page.component.ts
+++ src/app/_components/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked, AfterContentChecked } from '@angular/core';
 import { Router } from '@angular/router';
 import { faFacebookSquare, faTwitterSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
+import { interval, Subscription } from 'rxjs';
 
 
 @Component({
@@ -9,26 +10,40 @@ import { faFacebookSquare, faTwitterSquare, faYoutubeSquare } from '@fortawesome
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+
   faFacebookSquare = faFacebookSquare;
   faTwitterSquare = faTwitterSquare;
   faYoutubeSquare = faYoutubeSquare;
   animatecompbox = false;
   animaterenderingimage = false;
+  $checkInView: Subscription;
 
   constructor(private router: Router) { }
 
   @ViewChild('compwithbox') compwithbox: ElementRef;
   @ViewChild('renderingimage1') renderingimage1: ElementRef;
-  ngOnInit(): void {
+  ngOnDestroy() {
+    if (!this.$checkInView.closed) {
+      this.$checkInView.unsubscribe();
+    }
+  }
 
-    document.addEventListener('wheel', (e) => {
+  ngOnInit(): void {
+    const source = interval(200);
+    this.$checkInView = source.subscribe(() => {
       if (this.isElementInViewport(this.compwithbox.nativeElement)) {
         this.animatecompbox = true
       }
       if (this.isElementInViewport(this.renderingimage1.nativeElement)) {
         this.animaterenderingimage = true
       }
-    });
+      if (this.animatecompbox && this.animaterenderingimage) {
+        if (!this.$checkInView.closed) {
+          this.$checkInView.unsubscribe();
+        }
+      }
+    }
+    );
   }
 
   isElementInViewport(el) {
